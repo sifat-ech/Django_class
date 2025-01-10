201e3af0111064c10d725e5b5b99d5b8492cc492
safayet-ullah <safayetsifat117@gmail.com>
2025-01-10 19:22:13 +0600

    add registration form

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "Polls Administration"
+admin.site.index_title = "Welcome to Polls Admin Panel"
+
 admin.site.register(Question)
 admin.site.register(Choice)
